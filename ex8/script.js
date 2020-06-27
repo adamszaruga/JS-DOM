1bfa8469355f42718aadd7cc04a5fbbc2b8b660d
Nasiyra McCorkle <Nasiyra.mccorkle@gmail.com>
2020-06-27 11:18:22 -0400

    completed the exercise

--- ex8/script.js
+++ ex8/script.js
@@ -5,3 +5,35 @@
     BONUS) Add a functioning "delete" button to each task (HINT: use the keyword "this" in your delete button's click listener! Console log "this" to see what value it holds)
     SUPER BONUS) Make each task editable
 */
+
+
+
+var addTodo = function() {
+    var todo = document.getElementById('description');
+
+    if(todo.value != '') {
+        var newTodo = document.createElement('li');
+        newTodo.innerHTML = todo.value;
+        newTodo.className = "todo-item";
+    
+        todo.value = '';
+    
+        var list = document.getElementById('todo-list');
+        list.appendChild(newTodo);
+    }
+};
+
+var checkTodo = function(event) {
+    var doneTodo = event.target;
+    doneTodo.style.textDecoration = 'line-through';
+}
+
+
+var button = document.getElementById('add-button');
+button.addEventListener('click', addTodo);
+
+var todoItems = document.getElementsByClassName('todo-item');
+
+for(var i = 0; i < todoItems.length ; i++){
+    todoItems[i].addEventListener('click', checkTodo);
+}
